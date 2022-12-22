58aec449e02bdf63f3ddf9e776a02dd5a60b8e74
Eduardo Villalobo <eduardovillalobo23@gmail.com>
2022-12-22 12:34:41 -0300

    first commit

---
+++ polls/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+from django.utils import timezone
+
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        #filter pub_date__lte (less than or equal)
+
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': "No elegió una opción"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
