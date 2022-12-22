7ee0c18341932529f26519e402f63fd0cf41e265
Eduardo Villalobo <eduardovillalobo23@gmail.com>
2022-12-22 13:43:52 -0300

    settings

--- galeriadjango/settings.py
+++ galeriadjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2vrue3__oti(3n7p%a)fv&996*i*9@uwmhvye3kgz(q(7he4o'
+dontenv_file = os.path.join(BASE_DIR, ".env")
+if(os.path.isfile(dontenv_file)):
+    dotenv.load_dotenv(dontenv_file)
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
